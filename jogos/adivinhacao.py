3788f20917c4be675d58a3025652a55beff7f875
Alexandre Ximenes <alexandre.ximenes@livetouch.com.br>
2019-04-10 13:16:19 -0300

    jogo - nivel, pontuacao etc

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -2,14 +2,27 @@ import random
 
 print("********************************* Bem vindo no jogo de adivinhação *********************************")
 
-total_de_tentativas = 3
+total_de_tentativas = 0
 rodada = 1
 # numero_secreto = round(random.random() * 100)
 numero_secreto = round(random.randrange(1,101))
 
-for rodada in range(1, total_de_tentativas):
+print("Qual nivel de dificuldade?")
+print("(1) Facil (2) Medio (3) Dificil")
 
-    print("Tentativa {:03d} de {:03d}".format(rodada, total_de_tentativas))
+nivel = int(input("Defina o nivel: "))
+pontos = 100
+
+if nivel == 1:
+    total_de_tentativas = 10
+elif nivel == 2:
+    total_de_tentativas = 5
+else:
+    total_de_tentativas = 3
+
+for rodada in range(1, total_de_tentativas + 1):
+
+    print("Tentativa {:d} de {:d}".format(rodada, total_de_tentativas))
 
     chute_str = int(input("Digite o seu numero entre 1 e 100: "))
 
@@ -26,13 +39,15 @@ for rodada in range(1, total_de_tentativas):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou, parabens !!!!")
+        print("Você acertou e fez {:d}, parabens !!!!".format(pontos))
         break
     else:
         if maior:
             print("Voce nao acertou, seu chute foi maior que o numero secreto")
         elif menor:
             print("Voce nao acertou, seu chute foi menor que o numero secreto")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos -= pontos_perdidos
 
 
 
