9e09de042ed18bc1c56a14350621022989ed6496
Alexandre Ximenes <alexandre.ximenes@livetouch.com.br>
2019-04-12 13:17:35 -0300

    jogo - jogo da forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,6 +1,25 @@
 
 def jogar():
     print("********************************* Bem vindo no jogo da forca *********************************")
+    nao_enforcou = False
+    nao_acertou = False
+
+
+    palavra_secreta = 'banana'
+
+    while(not nao_enforcou and not nao_acertou):
+        chute = input("Informe uma letra ")
+        if chute != '':
+            # palavra_secreta.find()
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    print("Encontrei a letra {} na posição {}".format(letra, index))
+                index += 1
+
+
+
+
     print("******************************************** Fim do jogo *******************************************")
 
 if(__name__ == "__main__"):
